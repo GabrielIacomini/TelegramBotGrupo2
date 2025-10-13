ab220b1f37631360f649f5c68fa132662ee5cec9
GabrielIacomini <164112858+GabrielIacomini@users.noreply.github.com>
2025-10-13 20:43:27 -0300

    Init TelegramBot Repo

---
+++ src/main/java/com/example/TelegramBotGrupo2/TelegramBotGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.TelegramBotGrupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TelegramBotGrupo2/TelegramBotGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelegramBotGrupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
